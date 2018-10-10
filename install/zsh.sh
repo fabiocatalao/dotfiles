42600e7a6f5d7c3f5bfb60b318e921d688066fb0
Fabio Catalao <fabiocatalao@gmail.com>
2018-10-10 23:01:56 +0100

    Added gpakosz/.tmux settings

--- install/zsh.sh
+++ install/zsh.sh
@@ -7,6 +7,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 curl -L https://iterm2.com/shell_integration/zsh \-o ~/.iterm2_shell_integration.zsh
 curl -L git.io/antigen > antigen.zsh
 
+# Change default shell
 chsh -s $(which zsh)
+
+# Powerline fonts for tmux
+pip install --user git+git://github.com/Lokaltog/powerline --verbose
 
 source ~/.zshrc
